71770a088ddaedfb2d81da1e95db5f1adadd5bb9
Nik Skaily <Laito.Lawiet@gmail.com>
2019-12-23 23:33:42 +0200

    EffectController

---
+++ Assets/Scripts/Controllers/EffectsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NLW
+{
+    /// <summary> Контроллер эффектов </summary>
+    public class EffectsController : MainController
+    {
+        protected override void Init() { }
+    }
+}
